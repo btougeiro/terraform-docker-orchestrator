4803dcb8a784c879a98d7e40323a6faf3e491b38
Bruno Tougeiro <b.tougeiro@icloud.com>
2022-06-11 01:02:27 +0100

    Output name and id, added outputs description

--- outputs.tf
+++ outputs.tf
@@ -6,139 +6,125 @@
 # DOCKER CONFIG
 # -----------------------------------------------------------------------------
 
-output "docker_config_name" {
+output "config_name" {
   value = docker_config.this.name
   description = "User-defined name of the config."
 }
 
-output "docker_config_id" {
+output "config_id" {
   value = docker_config.this.id
+  description = "The ID of this resource."
 }
 
 # -----------------------------------------------------------------------------
 # DOCKER CONTAINER
 # -----------------------------------------------------------------------------
 
-output "docker_container_name" {
+output "container_name" {
   value = docker_container.this.name
+  description = "The name of the container."
 }
 
-output "docker_container_id" {
+output "container_id" {
   value = docker_container.this.id
-}
-
-output "docker_container_image" {
-  value = docker_container.this.image
-}
-
-output "docker_container_ports" {
-  value = docker_container.this.ports
-}
-
-output "docker_container_labels" {
-  value = docker_container.this.labels
-}
-
-output "docker_container_mounts" {
-  value = docker_container.this.mounts
+  description = "The ID of this resource."
 }
 
 # -----------------------------------------------------------------------------
 # DOCKER IMAGE
 # -----------------------------------------------------------------------------
 
-output "docker_image_name" {
+output "image_name" {
   value = docker_image.this.name
+  description = "The name of the Docker image, including any tags or SHA256 repo digests."
 }
 
-output "docker_image_id" {
+output "image_id" {
   value = docker_image.this.id
+  description = "The ID of this resource."
 }
 
 # -----------------------------------------------------------------------------
 # DOCKER NETWORK
 # -----------------------------------------------------------------------------
 
-output "docker_network_name" {
+output "network_name" {
   value = docker_network.this.name
+  description = "The name of the Docker network."
 }
 
-output "docker_network_id" {
+output "network_id" {
   value = docker_network.this.id
-}
-
-output "docker_network_driver" {
-  value = docker_network.this.driver
+  description = "The ID of this resource."
 }
 
 # -----------------------------------------------------------------------------
 # DOCKER PLUGIN
 # -----------------------------------------------------------------------------
 
-output "docker_plugin_name" {
+output "plugin_name" {
   value = docker_plugin.this.name
+  description = "Docker Plugin name."
 }
 
-output "docker_plugin_id" {
+output "plugin_id" {
   value = docker_plugin.this.id
-}
-
-output "docker_plugin_alias" {
-  value = docker_plugin.this.alias
+  description = "The ID of this resource."
 }
 
 # -----------------------------------------------------------------------------
 # DOCKER REGISTRY IMAGE
 # -----------------------------------------------------------------------------
 
-output "docker_registry_image_name" {
+output "registry_image_name" {
   value = docker_registry_image.this.name
+  description = "The name of the Docker image."
 }
 
-output "docker_registry_image_id" {
+output "registry_image_id" {
   value = docker_registry_image.this.id
+  description = "The ID of this resource."
 }
 
 # -----------------------------------------------------------------------------
 # DOCKER SECRET
 # -----------------------------------------------------------------------------
 
-output "docker_secret_name" {
+output "secret_name" {
   value = docker_secret.this.name
+  description = "User-defined name of the secret."
+
 }
 
-output "docker_secret_id" {
+output "secret_id" {
   value = docker_secret.this.id
+  description = "The ID of this resource."
 }
 
 # -----------------------------------------------------------------------------
 # DOCKER SERVICE
 # -----------------------------------------------------------------------------
 
- output "docker_service_name" {
+ output "service_name" {
    value = docker_service.this.name
+   description = "Name of the service."
  }
 
-output "docker_service_id" {
+output "service_id" {
   value = docker_service.this.id
-}
-
-output "docker_service_task_spec" {
-  value = docker_service.this.task_spec
+  description = "The ID of this resource."
 }
 
 # -----------------------------------------------------------------------------
 # DOCKER VOLUME
 # -----------------------------------------------------------------------------
 
-output "docker_volume_name" {
+output "volume_name" {
   value = docker_volume.this.name
+  description = "The name of the Docker volume (will be generated if not provided)."
 }
 
-output "docker_volume_id" {
+output "volume_id" {
   value = docker_volume.this.id
-}
-
-output "docker_volume_mountpoint" {
-  value = docker_volume.this.mountpoint
+  description = "The ID of this resource."
 }
