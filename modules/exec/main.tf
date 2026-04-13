8d34eb631155ba6aa89b46759c589023c434dd43
Bruno Tougeiro <b.tougeiro@icloud.com>
2026-04-13 01:32:43 +0100

    fix: resolve formatting and TFLint initialization issues in CI

--- modules/exec/main.tf
+++ modules/exec/main.tf
@@ -8,9 +8,9 @@ resource "docker_exec" "this" {
   container_id = each.value.container_id
   command      = each.value.command
 
-  detach = lookup(each.value, "detach", null)
-  env    = lookup(each.value, "env", null)
-  tty    = lookup(each.value, "tty", null)
-  user   = lookup(each.value, "user", null)
+  detach      = lookup(each.value, "detach", null)
+  env         = lookup(each.value, "env", null)
+  tty         = lookup(each.value, "tty", null)
+  user        = lookup(each.value, "user", null)
   working_dir = lookup(each.value, "working_dir", null)
 }
